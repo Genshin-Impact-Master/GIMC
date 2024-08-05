859dfec4ac8722c77e54482e36c6ebebc714fb0b
Zerosister_super <3788532552@qq.com>
2024-08-05 09:13:53 +0800

    添加隐藏功能测试

--- IRToArm/IRToArm.cpp
+++ IRToArm/IRToArm.cpp
@@ -7,14 +7,22 @@ void IRToArm::genModule() {
   // 准备工作
   smartOut(".arch", "armv7");
   smartOut(".fpu", "softvfp");
-  smartOut(".text");
-  smartOut(".align", 1);
-
-
 }
 
 void IRToArm::genFunction(Function *func) {
+  std::string &name = func->getName();
+  smartOut(".text");
+  smartOut(".align", 3);
+  smartOut(".global", name);
+  smartOut(".syntax", "unified");
+  smartOut(".thumb");
+  smartOut(".thumb_func");
+  smartOut(".type", name, "\%function");
+  
+  // @C++_Learn 两字符串比较
+  if (name.compare("main") == 0) {
 
+  }
 }
 
 void IRToArm::genBBlock(BBlock *bBlk) {
@@ -24,6 +32,8 @@ void IRToArm::genBBlock(BBlock *bBlk) {
 int IRToArm::getStackSize(Function *func) {
   int cnt = 0;
   INode<BBlock> *bBlkNode = func->getBBlockList().getHeadPtr();
+
+  // 处理指令偏移
   while (!bBlkNode->isEnd()) {
     bBlkNode = bBlkNode->getNext();
     BBlock *bBlk = bBlkNode->getOwner();
@@ -33,13 +43,14 @@ int IRToArm::getStackSize(Function *func) {
       Instruction *inst = instNode->getOwner();
       if (inst->getKind() == InstKind::Alloca) {
         baseTypePtr ty = inst->getType();
-        // 规定：a_ptr 一定存储在 a 的下方，a_ptr.offset + STACK_ALIGN 即为
+        // 规定：a_ptr 一定存储在 a 的下方，a_ptr.offset + STACK_ALIGN 即为 a 的地址
         stackOffset[inst] = cnt;
         cnt = alignsTo(cnt + 4, STACK_ALIGN);
         // 由于 Alloca 指令创建的一定为指针，故其取到的为所指空间大小
         cnt += alignsTo(ty->getSize(), STACK_ALIGN);
-        
       }
     }
   }
+
+  // 处理形参偏移，思考下，如若每次均是 alloca 则已经包含在上面
 }
