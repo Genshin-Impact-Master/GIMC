2d7a8fd49976dbf69bdab83cad58967e5c93c7c5
zerosister <2656091175@qq.com>
2024-08-10 17:30:13 +0800

    running

--- clearup.sh
+++ clearup.sh
@@ -3,9 +3,10 @@
 # Define the directories
 IR_DIR="./IR"
 PASS_DIR="./Pass"
+INCLUDE_DIR="./include"
 
 # Define the file patterns to delete
-PATTERNS=("example[0-9]*.ll" "main.png" "tmp_outputIR.ll")
+PATTERNS=("example[0-9]*.ll" "main.png" "tmp_outputIR.ll" "*.gch")
 
 # Function to delete files matching patterns in a directory
 delete_files() {
@@ -21,5 +22,18 @@ delete_files "$IR_DIR"
 
 # Delete files in Pass directory
 delete_files "$PASS_DIR"
+
+delete_files "$INCLUDE_DIR"
+
+for dir in "$INCLUDE_DIR"; do  
+    # 确保它是一个目录  
+    if [[ -d "$dir" ]]; then  
+        # 去除末尾的斜杠  
+        dirname="${dir%/}"  
+        # 假设你的操作是打印目录名  
+        delete_files $dirname  
+        # 在这里添加你的操作  
+    fi  
+done
 
 echo "Deletion completed."
